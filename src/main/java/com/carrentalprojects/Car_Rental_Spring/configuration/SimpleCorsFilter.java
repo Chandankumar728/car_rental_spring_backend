04fdbead74844c90f4b3043def4dab9c0927034e
chandankumar728 <chandankumar6299068@gmail.com>
2025-12-10 13:58:52 +0530

    new changes

---
+++ src/main/java/com/carrentalprojects/Car_Rental_Spring/configuration/SimpleCorsFilter.java
@@ -0,0 +1,50 @@
+package com.carrentalprojects.Car_Rental_Spring.configuration;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class SimpleCorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+
+        // Dynamically get the Origin header from the request
+        String originHeader = request.getHeader("origin");
+
+        // Set CORS headers
+        response.setHeader("Access-Control-Allow-Origin", originHeader);
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "*");  // allow all headers
+
+        // Handle preflight OPTIONS request
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
+    }
+@Override
+    public void init(jakarta.servlet.FilterConfig filterConfig) {
+        // No initialization required
+    }
+}
